d06083c487413255580bcb1f78ca0648c4ce4a5c
tiago.c.v17@gmail.com <tiago.c.v17@gmail.com>
2018-05-24 21:22:29 -0400

    Add static html page with handlebars

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const hbs = require('hbs');
 
-const app = express();
 const port = process.env.PORT || 3000;
 
+const app = express();
+
 app.use(bodyParser.json());
+app.set('view engine', 'hbs');
+
 
 app.get('/', (req, res) => {
-    res.send('Here goes static page');
+    res.render('index.hbs', {
+        pageTitle: 'URL Shortener Microserver',
+        pageDescription: 'Page description',
+        userStories: [
+            'I can pass a URL as a parameter and I will receive a shortened URL in the JSON response.',
+            'If I pass an invalid URL that doesn\'t follow the valid http://www.example.com format, the JSON response will contain an error instead.',
+            'When I visit that shortened URL, it will redirect me to my original link.'
+        ]
+    })
 });
 
 app.post('/new/:original_url', (req, res) => {
@@ -17,7 +31,6 @@ app.post('/new/:original_url', (req, res) => {
 
 app.get('/:id', (req, res) => {
     let url_id = req.params.id;
-
     res.status(200).send({url_id});
 });
 
