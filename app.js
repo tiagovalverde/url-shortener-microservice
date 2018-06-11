7acdba3f2ffc463cce3fb7e7eae8b44740040caf
tiago.c.v17@gmail.com <tiago.c.v17@gmail.com>
2018-06-10 21:43:19 -0400

    Update index page with new user stories info

--- app.js
+++ app.js
@@ -25,12 +25,7 @@ app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.render('index.hbs', {
-        pageTitle: 'URL Shortener Microserver',
-        userStories: [
-            'I can pass a URL as a parameter and I will receive a shortened URL in the JSON response.',
-            'If I pass an invalid URL that doesn\'t follow the valid http://www.example.com format, the JSON response will contain an error instead.',
-            'When I visit that shortened URL, it will redirect me to my original link.'
-        ]
+        pageTitle: 'API Project: URL Shortener Microservice'
     })
 });
 
